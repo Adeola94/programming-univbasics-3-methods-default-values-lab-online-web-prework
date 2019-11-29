0485a5dc4735d5679bf37a2ea79133335817c226
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-29 17:57:27 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,8 @@
-def meal_choice(veg1, veg2, protein = 'meat')
-  puts "What a nutritious meal!. A plate of #{protein} with #{veg1} and #{veg2}."
-end
+def meal_choice(veg1, veg2, protein = meat)
+  puts "what a nutritious meal!"
+  puts "A plate of #{protein} with #{veg2}"
+end 
 
 meal_choice("broccoli", "macaroni")
+
+  
